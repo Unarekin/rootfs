e7ae313c19541e1ed1108c4b89696d015018b64c
Erica Phelps <erica@blackspork.com>
2019-06-16 12:41:55 -0500

    Consolidated functions down to a single file, as all separated files were doing the same thing, which is pretty redundant. Also removes several file system calls, should speed up initialization.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,105 @@
+import * as defaultfs from 'fs';
 import * as path from 'path';
+import * as fsUtils from './utils/index';
 
-function root(dir, fs = require('fs')) {
+
+const functionsToHook = [
+  "access",
+  "accessSync",
+  "appendFile",
+  "appendFileSync",
+  "chmod",
+  "chmodSync",
+  "chown",
+  "chownSync",
+  "close",
+  "closeSync",
+  "copyFile",
+  "copyFileSync",
+  "createReadStream",
+  "createWriteStream",
+  "exists",
+  "existsSync",
+  "fchmod",
+  "fchmodSync",
+  "fchown",
+  "fchownSync",
+  "fdatasync",
+  "fdatasyncSync",
+  "fstat",
+  "fstatSync",
+  "fsync",
+  "fsyncSync",
+  "ftruncate",
+  "ftruncateSync",
+  "futimes",
+  "futimesSync",
+  "lchown",
+  "lchownSync",
+  "link",
+  "linkSync",
+  "lstat",
+  "lstatSync",
+  "mkdir",
+  "mkdirSync",
+  "mkdtemp",
+  "mkdtempSync",
+  "open",
+  "openSync",
+  "read",
+  "readFile",
+  "readFileSync",
+  "readSync",
+  "readdir",
+  "readdir",
+  "readdirSync",
+  "readdirSync",
+  "readlink",
+  "readlinkSync",
+  "realpath",
+  "realpathSync",
+  "rename",
+  "renameSync",
+  "rmdir",
+  "rmdirSync",
+  "stat",
+  "statSync",
+  "symlink",
+  "symlinkSync",
+  "truncate",
+  "truncateSync",
+  "unlink",
+  "unlinkSync",
+  "unwatchFile",
+  "utimes",
+  "utimesSync",
+  "watch",
+  "watchFile",
+  "write",
+  "writeFile",
+  "writeFileSync",
+  "writeSync"
+];
+
+
+function root(dir="/", fs = defaultfs) {
   let abs = path.resolve(dir);
-  
-  // console.log("Creation: ", abs);
-  let funcs = require("./functions")(abs);
+
+  let funcs: any = {};
   funcs.rootPath = dir;
   funcs.rootPathAbsolute = abs;
 
-  // console.log(funcs.rootPath);
-  // console.log(funcs.rootPathAbsolute);
 
+  functionsToHook.forEach((funcName: string): void => {
+    funcs[funcName] = function() {
+      fsUtils.setPathArgument(dir, arguments);
+      return defaultfs[funcName].apply(funcs, arguments);
+    }
+  });
 
 
   return Object.assign({}, fs, funcs);
 }
 
 
-//export { root as default };
 module.exports = root;
