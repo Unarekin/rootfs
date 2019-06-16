4b6fae88726a9ebab32b4c5d52b67eb0a4a5c552
Erica Phelps <erica@blackspork.com>
2019-06-16 02:34:34 -0500

    Updated file reading and tests -- removed symlink code

--- src/functions/read.ts
+++ src/functions/read.ts
@@ -15,9 +15,7 @@ let funcNames: string[] = [
   "open",
   "openSync",
   "readdir",
-  "readdirSync",
-  "realpath",
-  "realpathSync"
+  "readdirSync"
 ];
 
 
@@ -27,48 +25,6 @@ funcNames.forEach((funcName: string): void => {
     return fs[funcName].apply(null, arguments);
   }
 });
-
-// function readFile(): void {
-//   fsUtils.setPathArgument(rootPath, arguments);
-//   return fs.readFile.apply(this, arguments); 
-// }
-
-// function readFileSync(): string | Buffer {
-//   fsUtils.setPathArgument(rootPath, arguments);
-//   return fs.readFileSync.apply(this, arguments);
-// }
-
-// function readlink(): void {
-//   fsUtils.setPathArgument(rootPath, arguments);
-//   return fs.readlink.apply(this, arguments);
-// }
-
-// function readlinkSync(): string | Buffer {
-//   fsUtils.setPathArgument(rootPath, arguments);
-//   return fs.readlinkSync.apply(this, arguments);
-// }
-
-// function createReadStream(): fs.ReadStream {
-//   fsUtils.setPathArgument(rootPath, arguments);
-//   return fs.createReadStream.apply(this, arguments);
-// }
-
-
-// function open(): void {
-//   fsUtils.setPathArgument(rootPath, arguments);
-//   return fs.open.apply(this, arguments);
-// }
-
-// function openSync(): number {
-//   fsUtils.setPathArgument(rootPath, arguments);
-//   return fs.openSync.apply(this, arguments);
-// }
-
-
-// function readdir(): void {
-//   fsUtils.setPathArgument(rootPath, arguments);
-
-// }
 
 
 module.exports = function(root: string): Object {
